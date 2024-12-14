a3dc42e2ddcda1159292b4850cea1c97d48a9cc9
lseeger <leoseeger@proton.me>
2024-12-14 17:27:49 +0100

    added rotating led mode

--- src/led.rs
+++ src/led.rs
@@ -15,6 +15,7 @@ const NUMBER_OF_LEDS: usize = 60;
 #[derive(Serialize, Deserialize, Debug)]
 enum LedState {
     On,
+    Rotate,
     Off,
 }
 
@@ -26,6 +27,8 @@ pub struct Led {
     color: Color,
 
     percentage: u8,
+
+    speed: u8,
 }
 
 #[derive(Debug, Clone,Deserialize, Serialize)]
@@ -34,6 +37,7 @@ pub enum Color {
     Red,
     Green,
     Blue,
+    Purple,
     Yellow,
 }
 
@@ -44,6 +48,7 @@ impl Color {
             Color::Red => (255, 0, 0),
             Color::Green => (0, 255, 0),
             Color::Blue => (0, 0, 255),
+            Color::Purple => (255, 0, 255),
             Color::Yellow => (255, 255, 0),
         }
     }
@@ -57,6 +62,7 @@ impl PartialEq for LedState {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (LedState::On, LedState::On) => true,
+            (LedState::Rotate, LedState::Rotate) => true,
             (LedState::Off, LedState::Off) => true,
             _ => false,
         }
@@ -71,6 +77,7 @@ impl PartialEq for Color {
             (Color::Green, Color::Green) => true,
             (Color::Blue, Color::Blue) => true,
             (Color::Yellow, Color::Yellow) => true,
+            (Color::Purple, Color::Purple) => true,
             _ => false,
         }
     }
@@ -88,40 +95,67 @@ impl LedController {
     /// Function to initialize Led Controller and start a thread to listen for messages to update Led state
     /// led_pin: AnyOutputPin - GPIO Pin to which the LED is connected
     /// channel: CHANNEL0 - RMT Channel to use for LED is Channel 0
-    pub fn new(led_pin : AnyOutputPin, channel: CHANNEL0) -> LedController {
+    pub fn new<C:esp_idf_hal::rmt::RmtChannel>(led_pin : AnyOutputPin, channel: C) -> LedController {
         let (tx, rx) = std::sync::mpsc::channel::<Led>();
         thread:: spawn( move || {
             let mut led = Led {
                 led_state: LedState::Off,
                 color: Color::White,
                 percentage: 1,
+                speed: 0,
             };
+            let mut pixels = vec![RGB8::default(); NUMBER_OF_LEDS];
             let mut ws2812 = LedController::init_led(led_pin,channel);
             ws2812.write(std::iter::repeat(RGB8::default()).take(NUMBER_OF_LEDS)).unwrap();
             loop {
                 led = match rx.recv() {
                     Ok(new_led) => {
-                        if new_led == led {
-                            thread::sleep(std::time::Duration::from_secs(1));
-                            continue;
-                        }
                         let (r,g,b) = new_led.color.to_rgb();
                         match new_led.led_state {
+                            // If led already was on just continue and sleep for 1 second
                             LedState::On => {
-                                info!("Turning LED on: {:?}", new_led);
-                                let step = (100 / new_led.percentage) as usize;
-                                let pixels: Vec<RGB8> = (0..NUMBER_OF_LEDS)
-                                    .map(|i| {
-                                        if i % step == 0 {
-                                            RGB8::new(r, g, b)
-                                        } else {
-                                            RGB8::default()
-                                        }
+                                if(new_led != led) {
+                                    info!("Turning LED on: {:?}", new_led);
+                                    let step = (100 / new_led.percentage) as usize;
+                                    pixels = (0..NUMBER_OF_LEDS)
+                                        .map(|i| {
+                                            if i % step == 0 {
+                                                RGB8::new(r, g, b)
+                                            } else {
+                                                RGB8::default()
+                                            }
+                                        })
+                                        .collect();
+                                    ws2812.write(&pixels).unwrap();
+                                }
+                                thread::sleep(std::time::Duration::from_millis(1000));
+                            }
+                            LedState::Rotate => {
+                                if(led.led_state != LedState::Rotate || led.percentage != new_led.percentage){
+                                    info!("Rotating LED: {:?}", new_led);
+                                    let step = (100 / new_led.percentage) as usize;
+                                    pixels= (0..NUMBER_OF_LEDS)
+                                        .map(|i| {
+                                            if i % step == 0 {
+                                                RGB8::new(r, g, b)
+                                            } else {
+                                                RGB8::default()
+                                            }
                                     })
                                     .collect();
-                                info!("Pixels: {:?}", pixels);
-                                ws2812.write(pixels).unwrap();
-                                thread::sleep(std::time::Duration::from_millis(1000));
+                                    ws2812.write(&pixels).unwrap();
+                                    thread::sleep(std::time::Duration::from_millis(10000/new_led.speed as u64));
+                                }else{
+                                    pixels.as_mut_slice().rotate_right(1);
+                                    // Get all Pixels not set to default (Turned off) and set to given color
+                                    let default_rgb = RGB8::default();
+                                    pixels.iter_mut().for_each(|pixel| {
+                                        if *pixel != default_rgb {
+                                            *pixel = new_led.color.to_rgb().into();
+                                        }
+                                    });
+                                    thread::sleep(std::time::Duration::from_millis(10000/new_led.speed as u64));
+                                }
                             }
                             LedState::Off => {
                                 info!("Turning LED off: {:?}", new_led);
@@ -142,13 +176,12 @@ impl LedController {
         }
     }
 
-    fn init_led(led_pin: AnyOutputPin, channel: CHANNEL0) -> LedPixelEsp32Rmt<'static,RGB8, LedPixelColorGrb24> {
+    fn init_led<C: esp_idf_hal::rmt::RmtChannel>(led_pin: AnyOutputPin, channel: C) -> LedPixelEsp32Rmt<'static,RGB8, LedPixelColorGrb24> {
         Ws2812Esp32Rmt::new(channel,led_pin).unwrap()
     }
 
     pub fn set_led_state(&mut self, new_led : Led) {
         self.tx.send(new_led).unwrap();
     }
-
 
 }
